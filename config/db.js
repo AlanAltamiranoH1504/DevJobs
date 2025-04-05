d0c9dec4e5e3f3a5b67ccb768df74d5cbbc837ed
Alan Altamirano Hernandez <altamiranohernandezalan@gmail.com>
2025-04-04 21:11:51 -0600

    feat: add .env file and configure database connection
    - add environment variables in .env file
    
    - add MongoDB database connection file

---
+++ config/db.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+//Conexion a la base de datos
+mongoose.connect(process.env.DATABASE, {
+}).then(() =>{
+    console.log("Conexion correcta a la base de datos");
+}).catch((e) => {
+    console.log("Error en la conexion a la base de datos");
+    console.log(e)
+});
+//Excepcion por error en conexion a db
+mongoose.connection.on('error', (err) => {
+    console.log("Error en la conexion a la base de datos");
+    console.log(err);
+});
+
+export default mongoose;
--- index.js
+++ index.js
@@ -3,13 +3,20 @@
  * DevJobs: Plataforma de busqueda y publicacion de trabajos para desarrolladores
  * Autor: Altamirano Hernández Alan - 04 de abril de 2025
  */
+import mongoose from 'mongoose'
+import conexion from './config/db.js'
 import express from 'express';
+import cokieParser from 'cookie-parser';
+import session from 'express-session';
+import mongoStore from 'connect-mongo'
 import {engine} from 'express-handlebars';
 import router from "./routes/index.js";
 import {fileURLToPath} from "url"
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 import * as path from "node:path";
+import dotenv from "dotenv";
+dotenv.config();
 
 //Lavantamiento servidor
 const app = express();
@@ -24,6 +31,20 @@ app.set("view engine", "handlebars");
 
 //Habilitamos archivos publicos
 app.use(express.static(path.join(__dirname, "public")));
+
+//Habilitacion de sesion para la conexion a la base de datos
+app.use(cokieParser());
+app.use(session({
+    secret: process.env.SECRET,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: new mongoStore({
+        mongoUrl: process.env.DATABASE,
+        collectionName: 'sessions',
+        ttl: 14 * 24 * 60 * 60,
+    })
+}));
 
 //Uso de Routers
 app.use("/devjobs", router)
