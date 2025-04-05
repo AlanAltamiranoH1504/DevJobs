9b65bc04b37b10399e7f8fcf4286a65d79c736bf
Alan Altamirano Hernandez <altamiranohernandezalan@gmail.com>
2025-04-04 20:03:29 -0600

    feat: add handlebar layout and handlebar home

---
+++ controllers/homeController.js
@@ -0,0 +1,14 @@
+
+//Rutas del home controller
+const home =  (req, res) => {
+    res.render('home', {
+        nombrePagina: "DevJobs",
+        tagline: "Encuentra y publica trabajos para desarrolladores web",
+        barra: true,
+        boton: true,
+    });
+}
+
+export {
+    home
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 //Definicion de router general
 import express from "express";
+import {home} from "../controllers/homeController.js";
 const router = express.Router();
 
-router.get("/", (req, res) =>{
-    res.send("Conexion desde el router general ");
-});
+router.get("/", home);
 
 export default router;
