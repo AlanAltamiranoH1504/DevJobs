ba05b9a53dc23ff6efb7114909d3969414b563e0
Alan Altamirano Hernandez <altamiranohernandezalan@gmail.com>
2025-04-11 09:08:50 -0600

    feat: add cycle for rendering available vacancies
    - add vacancy query in homeController home method
    
    - add each loop for rendering vacancies in handlebar home

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,13 +1,16 @@
 //Rutas del home controller
-const home =  (req, res) => {
+import Vacante from "../models/Vacante.js";
+
+const home = async  (req, res) => {
+    const vacantes = await Vacante.find().lean();
     res.render('home', {
         nombrePagina: "DevJobs",
         tagline: "Encuentra y publica trabajos para desarrolladores web",
         barra: true,
         boton: true,
+        vacantes
     });
 }
-
 export {
     home
 }
